7135187c70e6a74f5218083beb75b378b8547eac
kolisettyvenkataaditya <kolisettyvenkata@changejar.in>
2025-02-19 12:14:45 +0530

    Remove unnessary files

--- build/spotless/spotlessJava/src/main/java/com/example/springboot/feature_report_generation/repository/ReportRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot.feature_report_generation.repository;
-
-import com.example.springboot.feature_report_generation.entity.Report;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReportRepository extends MongoRepository<Report, String> {}
